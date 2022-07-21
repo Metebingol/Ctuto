592814feb25aafcf107dcf235fbc042cc97dc777
unknown <Metterre@outlook.com.tr>
2022-07-21 16:27:52 +0300

    something deleted

--- ConditionalOperation/loops.c
+++ ConditionalOperation/loops.c
@@ -3,10 +3,13 @@
 
 int main(){
 
-
-    // will be continued 
-
-
-
+    int i = 1;
+    while(i <= 5){
+            printf("this is %d. loop in the while\n",i);
+            i++;    
+        }
+    for(int j = 0;j++;j<5){
+        printf("this is %d. loop in the while",i);
+    } 
     return 0;
 }
