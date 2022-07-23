95ae7bb6b93d004c82459ace1b3f35dae069aff4
unknown <Metterre@outlook.com.tr>
2022-07-23 13:58:33 +0300

    modified pointers

---
+++ someProject/ListShifter.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+// In this project, the array given will be reversed and it will return.
+
+
+int *reverseList(int *pointList,int size){
+    int reversedList[size];
+    
+
+
+
+    return NULL;
+}
+
+
+
+
+
+
+int main(){
+    int *a;
+    int b[5] = {124,3124,1312,2143,3123};
+    int c ={5};
+    a = reverseList(b,c);
+    printf("%x",a+1);
+
+    return 0;
+}
